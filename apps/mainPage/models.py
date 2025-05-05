952bf0c9f2867877f7b635bd2f298847e36763e5
Дмитрий <dimats2350@gmail.com>
2025-05-05 15:07:33 +0200

    models update

--- apps/mainPage/models.py
+++ apps/mainPage/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class WareHouse(models.Model):
+    wareHouseID = models.AutoField(primary_key=True)
+    address = models.TextField(unique=True, db_index=True)
+
+    def __str__(self):
+        return self.address
+
+class Product(models.Model):
+    productID = models.AutoField(primary_key=True)
+    wareHouse = models.ForeignKey(WareHouse, on_delete=models.CASCADE)
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return str(self.productID)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.BinaryField()
+
+class ProductDetail(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.TextField()
+
