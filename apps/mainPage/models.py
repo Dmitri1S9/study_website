27a995b53fdaa87308f409d3853b52553477982c
Дмитрий <dimats2350@gmail.com>
2025-08-04 14:26:55 +0200

    Big Beautiful Commit

--- apps/mainPage/models.py
+++ apps/mainPage/models.py
@@ -1,29 +1,64 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import MinValueValidator
 
-class WareHouse(models.Model):
-    wareHouseID = models.AutoField(primary_key=True)
-    address = models.TextField(unique=True, db_index=True)
 
-    def __str__(self):
-        return self.address
+class Warehouse(models.Model):
+    """
+    Модель для хранения информации о складе.
+
+    :param address - адрес склада, строка длиной до 250 символов.
+    """
+    address = models.CharField(max_length=250)
+
+
+class Employee(models.Model):
+    """
+    Модель для хранения информации о сотрудниках.
+
+    :param warehouse - связь с моделью Warehouse, указывающая на склад, где работает сотрудник.
+    :param salary - зарплата сотрудника, не может быть отрицательной.
+    :param is_manager - булево значение, указывающее, является ли сотрудник менеджером.
+    """
+    warehouse = models.ForeignKey(to=Warehouse, on_delete=models.CASCADE)
+    salary = models.IntegerField(validators=[MinValueValidator(0)])
+    is_manager = models.BooleanField(default=False)
+
 
 class Product(models.Model):
-    productID = models.AutoField(db_index=True, primary_key=True)
-    productName = models.TextField(unique=True)
-    wareHouse = models.ForeignKey(WareHouse, on_delete=models.CASCADE)
-    cost = models.IntegerField()
+    """
+    Модель для хранения информации о товаре.
 
-    def __str__(self):
-        return str(self.productID)
-
-    def __add__(self, other):
-        pass
-
-class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_images/')
-
-class ProductDetail(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    :param warehouse - связь с моделью Warehouse, указывающая на склад, где находится товар.
+    :param cost - стоимость товара, не может быть отрицательной.
+    :param image - изображение товара, загружается в папку 'products/'.
+    :param description - текстовое описание товара.
+    """
+    warehouse = models.ForeignKey(to=Warehouse, on_delete=models.CASCADE)
+    cost = models.IntegerField(validators=[MinValueValidator(0)])
+    image = models.ImageField(upload_to='products/')
     description = models.TextField()
 
+
+class Basket(models.Model):
+    """
+    Модель для хранения информации о корзине пользователя.
+
+    :param user - связь с моделью User, указывающая на пользователя, которому принадлежит корзина.
+    :param product - связь с моделью Product, указывающая на товар, добавленный в корзину.
+    """
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    """
+    Модель для хранения комментариев к товарам.
+
+    :param user - связь с моделью User, указывающая на пользователя, который оставил комментарий.
+    :param product - связь с моделью Product, указывающая на товар, к которому оставлен комментарий.
+    :param text - текст комментария.
+    """
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    text = models.TextField()
--- apps/mainPage/views.py
+++ apps/mainPage/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Product, ProductImage, ProductDetail
+from .models import Product
+from django.contrib.auth import logout
 
 # Create your views here.
 
 # Главная страница приложения
 def main(request):
-    products = Product.objects.all()  # выгружаем все продукты из БД
-    images = ProductImage.objects.all()
-    details = ProductDetail.objects.all()
-    return render(request, 'mainPage/main.html', {
-        'products': products, 'images': images, 'details': details
-    })
+
+    # action_products = Product.objects.filter(
+    #     productaction__isnull=False
+    # ).prefetch_related('productimage_set', 'productdetail_set').distinct()
+
+    # return render(request, 'mainPage/main.html', {'products': action_products})
+    return render(request, 'mainPage/main.html')
 
