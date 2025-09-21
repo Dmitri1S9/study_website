651bd51ef91647ec151a84776520b20d8f054f60
Дмитрий <dimats2350@gmail.com>
2025-09-21 14:23:47 +0200

    post_and_get_tests

---
+++ apps/tracker/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Title(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    # all data must be in english
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name="characters")
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class ParsingMaterials(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="parsing_materials")
+    link = models.URLField(null=False, blank=False)
